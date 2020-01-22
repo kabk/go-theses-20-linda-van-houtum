be69fbefaccdd5bcf35a7b58ee57b56c3994b8a6
Agunua <58515278+Agunua@users.noreply.github.com>
2020-01-22 17:49:06 +0100

    Type sequense addition typewriter

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-// $( "button" ).click(function() {
-//   $( ".potato" ).toggle( "slow" );
-// });
+
+
+  // $("#five-content").click(function(){
+  //       if ( $(this).hasClass('Offline') )   $(this).removeClass('Offline').addClass('Activebook');
+  //       else    $(this).addClass('Offline')
+  // })
+
+
+
+
+
+// $('#five-content').click(function(ev) {
+//   $('.typing').removeClass('disable');
+//   $(this).addClass('active')
+//           if ( $(this).hasClass('Offline') )   $(this).removeClass('Offline').addClass('Activebook');
+//         else    $(this).addClass('Offline')
+// })
 
 
 
@@ -62,13 +76,13 @@ $(document).ready(function(){
   $('#img1').click(function(){
   $('#img1').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open1').click(function(){
   $('#img1').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
@@ -76,13 +90,13 @@ $('#open1').click(function(){
   $('#img2').click(function(){
   $('#img2').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open2').click(function(){
   $('#img2').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
@@ -90,13 +104,13 @@ $('#open2').click(function(){
   $('#img3').click(function(){
   $('#img3').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open3').click(function(){
   $('#img3').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
@@ -104,231 +118,231 @@ $('#open3').click(function(){
   $('#img4').click(function(){
   $('#img4').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open4').click(function(){
   $('#img4').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
   $('#img5').click(function(){
   $('#img5').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open5').click(function(){
   $('#img5').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
   $('#img6').click(function(){
   $('#img6').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open6').click(function(){
   $('#img6').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img7').click(function(){
   $('#img7').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open7').click(function(){
   $('#img7').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img8').click(function(){
   $('#img8').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open8').click(function(){
   $('#img8').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img9').click(function(){
   $('#img9').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open9').click(function(){
   $('#img9').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
   $('#img10').click(function(){
   $('#img10').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open10').click(function(){
   $('#img10').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img11').click(function(){
   $('#img11').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open11').click(function(){
   $('#img11').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img12').click(function(){
   $('#img12').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open12').click(function(){
   $('#img12').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img13').click(function(){
   $('#img13').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open13').click(function(){
   $('#img13').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img14').click(function(){
   $('#img14').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open14').click(function(){
   $('#img14').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img15').click(function(){
   $('#img15').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open15').click(function(){
   $('#img15').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img16').click(function(){
   $('#img16').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open16').click(function(){
   $('#img16').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img17').click(function(){
   $('#img17').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open17').click(function(){
   $('#img17').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img18').click(function(){
   $('#img18').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open18').click(function(){
   $('#img18').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img19').click(function(){
   $('#img19').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open19').click(function(){
   $('#img19').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
 
   $('#img20').click(function(){
   $('#img20').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open20').click(function(){
   $('#img20').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img21').click(function(){
   $('#img21').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open21').click(function(){
   $('#img21').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
 
   $('#img22').click(function(){
   $('#img22').animate({
     right: $(window).width()*-1
-  }, 1000)
+  }, 700)
 })
 
 $('#open22').click(function(){
   $('#img22').animate({
     right: 0
-  }, 1000)
+  }, 700)
 })
